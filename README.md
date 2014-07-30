3422863fab6e001932ba655f5d1428db0d950d43
foolchi <foolchi@outlook.com>
2014-07-30 16:00:00 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+###Google App Engine Test
+This is a test for google app engine using golang
--- hello.go
+++ hello.go
@@ -1,10 +1,11 @@
 package hello
 
 import (
-	//"appengine"
-	//"appengine/user"
 	"fmt"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
@@ -12,5 +13,25 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "你好!")
+	// [START new_context]
+	c := appengine.NewContext(r)
+	// [END new_context]
+	// [START get_current_user]
+	u := user.Current(c)
+	// [END get_current_user]
+	// [START if_user]
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	// [END if_user]
+	// [START output]
+	fmt.Fprintf(w, "Hello, %v!", u)
+	// [END output]
 }
